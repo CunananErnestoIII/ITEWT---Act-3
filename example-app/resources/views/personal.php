11a2cdff390746190f53210be31dbb50867d6476
Ernesto III Cunanan <ernestoiii.cunanan@letran.edu.ph>
2022-03-14 17:51:34 +0800

    .

---
+++ example-app/resources/views/personal.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <title>Personal Information</title>
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather&display=swap" rel="stylesheet">
+</head>
+<body>
+    <div>
+        <h1><b>Personal Information</b></h1>
+        <div><b>Name:</b> Cunanan, Ernesto III D.</div>
+        <div><b>Birthday:</b> April 5, 2001</div>
+        <div><b>Gender:</b> Male</div>
+        <div><b>Birth place:</b> Manila</div>
+        <div><b>School:</b> Colegio de San Juan de Letran</div>
+        <div><b>Course:</b> Bachelor of Science in Information Technology</div>
+        <div><b>Year:</b> 3rd Year</div>
+        <div><b>Email:</b> ernestoiii.cunanan@letran.edu.ph</div>
+    </div>
+</body>
+</html>
